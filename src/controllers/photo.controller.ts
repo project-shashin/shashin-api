ee3f130dfe91264a2ce3cc2735a4c0c9af57eac2
James Andrews <jamesmandrews@users.noreply.github.com>
2021-11-03 16:38:19 +0900

    added some spacing

--- src/controllers/photo.controller.ts
+++ src/controllers/photo.controller.ts
@@ -6,17 +6,21 @@ const generateDbClient = () => {
 }
 
 export const PhotoDelete = async (request: Request, response: Response, next: NextFunction) => {
+
   const client = generateDbClient();
 
 }
 
 export const PhotoGet = async (request: Request, response: Response, next: NextFunction) => {
+
   const prisma = generateDbClient();
   const allPhotos = await prisma.photo.findMany()
   response.status(200).json(allPhotos)
+
 }
 
 export const PhotoGetOne = async (request: Request, response: Response, next: NextFunction) => {
+
   const prisma = generateDbClient();
   const params = request.params;
 
@@ -30,9 +34,11 @@ export const PhotoGetOne = async (request: Request, response: Response, next: Ne
   let responseCode: number = (!photo) ? 204 : 200;
 
   response.status(responseCode).json(result);
+
 }
 
 export const PhotoPost = async (request: Request, response: Response, next: NextFunction) => {
+
   const prisma = generateDbClient();
   const data = request.body;
 
@@ -44,9 +50,11 @@ export const PhotoPost = async (request: Request, response: Response, next: Next
   // 403 resource exists for now on error, we'll correct later.
   let responseCode: number = (!photo) ? 403 : 201;
   response.status(responseCode).json(result);
+
 }
 
 export const PhotoPut = async (request: Request, response: Response, next: NextFunction) => {
+
   const prisma = generateDbClient();
   const data = request.body;
 
