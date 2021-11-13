4cded32ddd23c12632c6756e1095db485d09b896
James Andrews <jamesmandrews@users.noreply.github.com>
2021-11-13 20:54:29 +0900

    remove bad import

--- src/middleware/response-body.middleware.ts
+++ src/middleware/response-body.middleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction, response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 export const responseBodyGenerator = function(request: Request, response: Response, next: NextFunction) {
   const originalJsonFunction = response.json;
